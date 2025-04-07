880a2b7d0a753ff4e1e3e47c3588adf23bb25676
Zark <jeremie@downtownstudio.ch>
2025-04-07 15:47:30 +0200

    update

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+def main():
+    path_to_file = '/mnt/c/Users/jblaser/scripts/bookbot/books/frankenstein.txt'
+    book_text = get_book_text(path_to_file)
+    print(f'{book_text}')
+
+if __name__ == '__main__':
+    main()
